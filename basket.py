dac3fddbdf63c6b6dac923c77c351715b7912367
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2023-04-01 10:50:08 +0200

    Basket completed

--- basket.py
+++ basket.py
@@ -1,5 +1,4 @@
 from item import Item
-from pprint import pprint
 from utils.colors import Bcolors
 
 
@@ -18,7 +17,7 @@ class Basket:
     def view_basket(self):
         if self.contents:
             # print(self.contents)
-            print(f"{Bcolors.OKGREEN + Bcolors.BOLD}Basket total: {self.count_total()}\n"
+            print(f"{Bcolors.OKGREEN + Bcolors.BOLD}Basket total: {self.count_total()} $\n"
                   f"Items: {Bcolors.ENDC}")
             for k, v in self.contents.items():
                 print(f"{Bcolors.OKGREEN} - {k.capitalize()} - Unit price: {v['item'].get_price}, amount: {v['amount']}"
@@ -31,9 +30,17 @@ class Basket:
         item_name = input("Please provide an item name: ")
         # print(self.contents)
         # print(self.contents.get(item_name))
-        if item_name in self.contents.keys():
+        while True:
+            item_amount = input("Please provide item amount: ")
+            try:
+                if int(item_amount) > 0:
+                    item_amount = int(item_amount)
+                    break
+            except ValueError:
+                print(f"{Bcolors.FAIL}The amount must be a positive number!{Bcolors.ENDC}\n")
 
-            self.contents[item_name]["amount"] += 1
+        if item_name in self.contents.keys():
+            self.contents[item_name]["amount"] += item_amount
         else:
             while True:
                 item_price = input("Please provide an item price: ")
@@ -46,17 +53,39 @@ class Basket:
             item = Item(item_name, item_price)
             self.contents[item_name] = dict()
             self.contents[item_name]["item"] = item
-            self.contents[item_name]["amount"] = 1
-        print(f"{Bcolors.OKGREEN}----- One {item_name.capitalize()} added to the basket{Bcolors.ENDC}\n")
+            self.contents[item_name]["amount"] = item_amount
+        print(f"{Bcolors.OKGREEN}----- {item_amount} {item_name.capitalize()} added to the basket{Bcolors.ENDC}\n")
 
     def delete_item(self):
         item_name = input("Please provide an item name: ")
         selected_item = self.contents.get(item_name)
         if selected_item is None:
-            print("Nothing to delete")
+            print(f"{Bcolors.FAIL} No item named {item_name.capitalize()} found in the basket. Nothing to delete."
+                  f"{Bcolors.ENDC}\n")
         else:
-            if self.contents[item_name]["amount"] > 0:
-                self.contents[item_name]["amount"] -= 1
-            else:
-                self.contents.pop(selected_item)
-            print(f"{Bcolors.WARNING}One {item_name.capitalize()} has been deleted.{Bcolors.ENDC}\n")
+            while True:
+                item_amount = input("Please provide item amount: ")
+                try:
+                    if int(item_amount) > 0:
+                        item_amount = int(item_amount)
+                        break
+                except ValueError:
+                    print(f"{Bcolors.FAIL}The amount must be a positive number!{Bcolors.ENDC}\n")
+
+            deleted_items = 0
+            for i in range(item_amount):
+                try:
+                    if self.contents[item_name]["amount"] > 0:
+                        self.contents[item_name]["amount"] -= 1
+                        deleted_items += 1
+                        if self.contents[item_name]["amount"] == 0:
+                            self.contents.pop(item_name)
+
+                    else:
+                        deleted_items = 1
+                        self.contents.pop(item_name)
+                except KeyError:
+                    # print(f"{Bcolors.OKBLUE}No more items {item_name.capitalize()} to delete\n{Bcolors.ENDC}")
+                    break
+
+            print(f"{Bcolors.WARNING}{deleted_items} {item_name.capitalize()} items have been deleted.{Bcolors.ENDC}\n")
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from basket import Basket
+from utils.colors import Bcolors
 
 
 def run_app():
     basket = Basket()
     is_on = True
     while is_on:
-        print("###############################")
+        print(f"{Bcolors.HEADER}###############################{Bcolors.ENDC}")
         print("""Add item to the basket: type a\nDelete item from the basket: type d\nQuit the program: q
 View basket: v\n""")
         user_input = input("Please provide your command: ").lower()

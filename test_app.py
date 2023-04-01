235acb48fcc3815cdff90a8b6c1e9d0836775593
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2023-04-01 14:29:50 +0200

    some tests added

--- test_app.py
+++ test_app.py
@@ -1,8 +1,24 @@
 import pytest
-from main import run_app
+
+from basket import Basket, check_item_name_is_number, check_if_num_positive
 
 
-def test_app_quits():
-    run_app()
+def test_check_item_name_is_number_returns_false():
+    assert not check_item_name_is_number("test")
 
-    pass
+
+def test_check_item_name_is_number_returns_true_provided_int():
+    assert check_item_name_is_number(12)
+
+
+def test_check_item_name_is_number_returns_true_provided_int_str():
+    assert check_item_name_is_number("12")
+
+
+def test_check_if_num_positive_returns_number():
+    assert check_if_num_positive("352", int) == 352
+
+
+def test_check_if_num_positive_returns_none():
+    assert check_if_num_positive("elo", int) is None
+
